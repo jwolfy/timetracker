0d1494a9c86fe0c79370904676f899336244a420
jwolfy <84108269+jwolfy@users.noreply.github.com>
2021-07-14 01:13:14 +0300

    Initialize Vue frontend app

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello timetracker"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>

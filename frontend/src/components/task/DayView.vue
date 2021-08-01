20a1a4eb6d59a5a0c94e0444b190abefdbf3016f
jwolfy <84108269+jwolfy@users.noreply.github.com>
2021-08-01 12:30:14 +0300

    Show total hours for day

--- frontend/src/components/task/DayView.vue
+++ frontend/src/components/task/DayView.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <div class="date is-size-4 has-text-grey">
-      {{ date }}
+    <div class="container day-title">
+      <div class="date is-size-4 has-text-grey">
+        {{ date }}
+      </div>
+      <div class="total-for-day is-size-5 has-text-right">
+        {{ totalForPeriod(data) }}
+      </div>
     </div>
     <div v-for="(issueData, issue_id) in data" :key="issue_id">
       <IssueView :data="issueData" />
@@ -24,3 +29,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.day-title {
+  display: flex;
+  align-items: center;
+}
+
+.date {
+  flex: 1
+}
+
+.total-for-day {
+  flex: 1;
+}
+</style>
